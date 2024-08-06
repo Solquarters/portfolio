3530196cec7dc3a8d8735a48891d2727c3aec9a2
Solquarters <rom.kabucov@gmail.com>
2024-08-06 14:02:45 +0200

    set up project structure, working on header and landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LegalnoticeComponent } from './legalnotice/legalnotice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: MainContentComponent},
+    {path: 'legal-notice', component: LegalnoticeComponent}
+];
