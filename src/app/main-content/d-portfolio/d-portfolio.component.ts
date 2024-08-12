5cae50033a8c8a44fa58afa727452572d1d8ffbc
Solquarters <rom.kabucov@gmail.com>
2024-08-12 18:28:23 +0200

    portfolio start and responsive

--- src/app/main-content/d-portfolio/d-portfolio.component.ts
+++ src/app/main-content/d-portfolio/d-portfolio.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-d-portfolio',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './d-portfolio.component.html',
   styleUrl: './d-portfolio.component.scss'
 })
 export class DPortfolioComponent {
+
+
+  projects =  [
+    {
+      name: "Join",
+      toolsUsed: "JavaScript | HTML | CSS | Firebase",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      liveTestUrl: "https://code-crafters-join-3.developerakademie.net/06_09_GruppenProjekt_Join_ProjektOrdner/02summary.html",
+      githubLink: "https://github.com/Solquarters/Join_Gruppe3"
+    },
+    {
+      name: "Join",
+      toolsUsed: "JavaScript | HTML | CSS | Firebase",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      liveTestUrl: "https://code-crafters-join-3.developerakademie.net/06_09_GruppenProjekt_Join_ProjektOrdner/02summary.html",
+      githubLink: "https://github.com/Solquarters/Join_Gruppe3"
+    },
+    
+  ]
+
+
 
 }
