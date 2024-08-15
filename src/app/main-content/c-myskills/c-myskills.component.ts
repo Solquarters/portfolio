08d36d5619b11b715bdc22194ed6558f0130f36b
Solquarters <rom.kabucov@gmail.com>
2024-08-16 01:29:47 +0200

    rather insane further interest bubble, responsive solved very inefficiently

--- src/app/main-content/c-myskills/c-myskills.component.ts
+++ src/app/main-content/c-myskills/c-myskills.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-c-myskills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './c-myskills.component.html',
   styleUrl: './c-myskills.component.scss'
 })
 export class CMyskillsComponent {
+
+  isHovered = false;
 
 }
