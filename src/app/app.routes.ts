4c4c352e72213419f645fcf1ff601a0e3d6bc3eb
Solquarters <rom.kabucov@gmail.com>
2024-08-16 12:19:56 +0200

    added privacy policy and responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { LegalnoticeComponent } from './legalnotice/legalnotice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
 
     {path: '', component: MainContentComponent},
-    {path: 'legal-notice', component: LegalnoticeComponent}
+    {path: 'legal-notice', component: LegalnoticeComponent},
+    {path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
