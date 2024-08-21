230560baab7c25500094332405348844103d37ce
Solquarters <rom.kabucov@gmail.com>
2024-08-21 20:18:01 +0200

    translate in process- about me

--- src/app/main-content/b-aboutme/b-aboutme.component.ts
+++ src/app/main-content/b-aboutme/b-aboutme.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-b-aboutme',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './b-aboutme.component.html',
   styleUrl: './b-aboutme.component.scss'
 })
 export class BAboutmeComponent {
 
+
+
+
+ 
+  translate = inject(TranslationService);
 }
