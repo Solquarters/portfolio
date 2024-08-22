b77efa94b2f8cbbd2fc292c2681f966f704618f3
Solquarters <rom.kabucov@gmail.com>
2024-08-22 18:36:21 +0200

    legal notice translate

--- src/app/legalnotice/legalnotice.component.ts
+++ src/app/legalnotice/legalnotice.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legalnotice',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './legalnotice.component.html',
   styleUrl: './legalnotice.component.scss'
 })
 export class LegalnoticeComponent {
 
+  translate = inject(TranslationService);
 }
